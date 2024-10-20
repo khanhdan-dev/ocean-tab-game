bfb14ff94914dfdc4e3cf649692acd2fe9eb43f3
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.comexport>
2024-10-20 10:41:39 +0700

    enable cores

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
-  // Enable CORS
-  const corsOptions: CorsOptions = {
-    origin: ['https://ocean-tab-game.vercel.app/', 'http://localhost:3000'], // Allow requests from the frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  };
-  app.enableCors(corsOptions);
+
+  // app.enableCors();
   const port = process.env.PORT || 5000;
   await app.listen(port);
 }
