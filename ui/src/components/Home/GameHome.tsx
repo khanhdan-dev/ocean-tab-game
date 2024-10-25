d93dcaadeb52226f30be7888b5a6febf05d9d67d
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.comexport>
2024-10-25 22:08:47 +0700

    remove unused import

--- ui/src/components/Home/GameHome.tsx
+++ ui/src/components/Home/GameHome.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
