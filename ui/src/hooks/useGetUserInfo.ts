f3d484cb537e93401db035ccbf23636b9bb9cb7c
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.comexport>
2024-10-25 22:19:11 +0700

    update useGetUserInfo

--- ui/src/hooks/useGetUserInfo.ts
+++ ui/src/hooks/useGetUserInfo.ts
@@ -1,16 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { userService } from "kan/services/userService";
-import { useGetAllUsers } from "./useGetAllUsers";
 import { useCreateUserMutate } from "./useCreateUserMutate";
 import { ITelegramUserInfo } from "kan/types";
 
 export const useGetUserInfo = (telegramUser: ITelegramUserInfo) => {
-  const { data: userList } = useGetAllUsers();
-  const { mutate: createUserMutate } = useCreateUserMutate();
+  const { mutateAsync: createUserMutate } = useCreateUserMutate();
 
   return useQuery<ITelegramUserInfo | null>({
-    queryKey: ["getUserInfo", userList, telegramUser],
+    queryKey: ["getUserInfo", telegramUser],
     queryFn: async () => {
+      const userList = await userService.getAllUsers();
       if (userList) {
         const existedData = userList?.find((u) => u.id === telegramUser?.id);
 
@@ -20,25 +19,24 @@ export const useGetUserInfo = (telegramUser: ITelegramUserInfo) => {
           );
           return { ...userFromBackend, isNewUser: true };
         } else {
-          return new Promise<ITelegramUserInfo | null>((resolve) => {
-            createUserMutate(
-              {
-                ...telegramUser,
-                turns: 100, // Default initial value
+          return createUserMutate(
+            {
+              ...telegramUser,
+              turns: 100, // Default initial value
+            },
+            {
+              onSuccess: async (data) => {
+                const userInfo = await userService.getUserInformation(
+                  String(data.id)
+                );
+                return userInfo;
               },
-              {
-                onSuccess: async (data) => {
-                  const userInfo = await userService.getUserInformation(
-                    String(data.id)
-                  );
-                  resolve(userInfo);
-                },
-                onError: () => {
-                  resolve(null); // Handle the case when the mutation fails
-                },
-              }
-            );
-          });
+              onError: (err) => {
+                console.error("err: ", err.message);
+                return null; // Handle the case when the mutation fails
+              },
+            }
+          );
         }
       }
       return null; // If userList is not defined, return null
