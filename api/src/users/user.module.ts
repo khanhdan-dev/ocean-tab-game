8dd005df01872bc4d9f4b8e921e45c5495738c1a
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.comexport>
2024-11-02 12:10:11 +0700

    api: update cron user turns every day utc-0

--- api/src/users/user.module.ts
+++ api/src/users/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ScheduleModule.forRoot(),
   ],
   controllers: [UserController],
   providers: [UserService],
--- api/src/users/user.service.ts
+++ api/src/users/user.service.ts
@@ -4,6 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { Cron } from '@nestjs/schedule';
 
 @Injectable()
 export class UserService {
@@ -33,7 +34,30 @@ export class UserService {
     return user.save();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  @Cron('0 0 * * *', {
+    timeZone: 'UTC', // Set time zone to UTC
+  })
+  async handleCronAddTurnsEveryDay() {
+    const userList: User[] = await this.userModel.find().exec();
+
+    const bulkOps = userList.map((u) => ({
+      updateOne: {
+        filter: { id: u.id }, // Use the MongoDB ObjectId
+        update: { $set: { turns: u.turns + 20 } },
+      },
+    }));
+
+    if (bulkOps.length > 0) {
+      await this.userModel.bulkWrite(bulkOps);
+    }
+  }
+
+  async remove(id: number) {
+    // Change number to string for ObjectId
+    const result = await this.userModel.deleteOne({ id: id }).exec();
+    if (result.deletedCount === 0) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return `User with ID ${id} removed successfully`;
   }
 }
